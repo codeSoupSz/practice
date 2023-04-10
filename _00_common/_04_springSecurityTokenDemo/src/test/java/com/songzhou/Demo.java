d81cbe6d8e52f263036ff27c7cfa6d8d2510f7e7
zhousong <1465056501@qq.com>
2023-04-10 22:41:58 +0800

    02-配置一个加密器，数据库密码不存储明文

--- _00_common/_04_springSecurityTokenDemo/src/test/java/com/songzhou/Demo.java
+++ _00_common/_04_springSecurityTokenDemo/src/test/java/com/songzhou/Demo.java
@@ -2,17 +2,26 @@ package com.songzhou;
 
 import com.songzhou.domain.User;
 import com.songzhou.mapper.UserMapper;
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.BEncoderStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 public class Demo {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
     @Test
     public void test01(){
         User user = userMapper.selectById(2);
         System.out.println("user = " + user);
+    }
+    @Test
+    public void testEncrypt(){
+        String encode = passwordEncoder.encode("123456");
+        System.out.println("encode = " + encode);
     }
 }
